46e5141f2f6826c4aaed14b33edb9d63581aa5f7
Marcos Tanaka <marcosatanaka@gmail.com>
2026-04-01 11:12:30 -0300

    Scripts to publish what's new

---
+++ musicbox-publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add whats-new/musicbox-ios.html whats-new/musicbox-macos.html
+git commit -m "What's New in MusicBox"
+git push origin master
---
+++ play-publish.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git add whats-new/play.html whats-new/play-macos.html
+git commit -m "What's New in Play"
+git push origin master
